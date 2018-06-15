cc677e22198b50ac3d99ea03f22f59c83ca3ea85
Lawrence Neal <nealla@lwneal.com>
2018-06-15 13:30:52 -0700

    Better formatting for file listings

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='gnomehat',
-        version='0.4.2',
+        version='0.4.3',
         description='GnomeHat makes it easy to run experiments.',
         author='Larry Neal',
         author_email='nealla@lwneal.com',
