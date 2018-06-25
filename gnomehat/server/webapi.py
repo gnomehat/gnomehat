1a6a1a9248bec735151f805a95a3d248bc164536
Lawrence Neal <nealla@lwneal.com>
2018-06-25 11:25:48 -0700

    Disable CUDA when running tensorboard visualization

--- gnomehat/server/webapi.py
+++ gnomehat/server/webapi.py
@@ -226,7 +226,7 @@ def spawn_tensorboard(logdir):
 
     portnum = random.randint(21000, 21999)
     # TODO proper input sanitizing and process pool and resource management and and ...
-    cmd = 'tensorboard --logdir {} --port {} & >/dev/null'.format(logdir, portnum)
+    cmd = 'CUDA_VISIBLE_DEVICES="" tensorboard --logdir {} --port {} & >/dev/null'.format(logdir, portnum)
     print("Running {}".format(cmd))
     os.system(cmd)
     return portnum
