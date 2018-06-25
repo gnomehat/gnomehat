f4cb4a74df973ef2c299c1c393f197f54eda51c3
Lawrence Neal <nealla@lwneal.com>
2018-06-25 14:35:57 -0700

    Auto-install websocketd with post-install hook
    Not the cleanest way to install it, but it works for now.
    The real goal is to replace websocketd with a pure Python utility.

--- setup.py
+++ setup.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from setuptools.command.install import install
+import os
+import requests
+import subprocess
+
+WEBSOCKETD_URL = 'https://github.com/joewalnes/websocketd/releases/download/v0.3.0/websocketd-0.3.0-linux_amd64.zip'
+WEBSOCKETD_FILENAME = 'websocketd-0.3.0-linux_amd64.zip'
+WEBSOCKETD_CHECKSUM = '03b8d90b4ac1f58469965c133cf3dd9c43dc0811e525b2319df578e7057db5b4'
+
+class PostInstallCommand(install):
+    def run(self):
+        install_websocketd('~/bin')
+        install.run(self)
+
+
+def install_websocketd(bin_dir):
+    bin_dir = os.path.expanduser(bin_dir)
+    os.makedirs(bin_dir, exist_ok=True)
+    filepath = os.path.join(bin_dir, WEBSOCKETD_FILENAME)
+    subprocess.run(['wget', '--no-clobber', '-P', bin_dir, WEBSOCKETD_URL])
+    subprocess.run(['unzip', '-d', bin_dir, filepath])
+
 
 setup(name='gnomehat',
-        version='0.4.6',
+        version='0.4.7',
         description='GnomeHat makes it easy to run experiments.',
         author='Larry Neal',
         author_email='nealla@lwneal.com',
@@ -35,4 +57,7 @@ setup(name='gnomehat',
           "flask",
           "pytz",
       ],
+      cmdclass={
+        'install': PostInstallCommand,
+      },
 )
