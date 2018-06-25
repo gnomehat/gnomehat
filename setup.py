a8b504a26f5e55161a82d97d4b7d515250778bed
Lawrence Neal <nealla@lwneal.com>
2018-06-24 19:46:00 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='gnomehat',
-        version='0.4.5',
+        version='0.4.6',
         description='GnomeHat makes it easy to run experiments.',
         author='Larry Neal',
         author_email='nealla@lwneal.com',
