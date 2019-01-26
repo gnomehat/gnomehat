195efcb31ff68ccce96051937ef4721299eaf2e0
Lawrence Neal <nealla@lwneal.com>
2019-01-26 11:51:09 -0800

    Bump version number

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def install_websocketd(bin_dir):
 
 
 setup(name='gnomehat',
-    version='0.6.1',
+    version='0.6.2',
     description='GnomeHat: easy experiment control',
     author='Larry Neal',
     author_email='nealla@lwneal.com',
