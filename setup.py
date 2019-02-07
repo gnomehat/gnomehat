77c98c755448548d1f78a2651897a4280fd71a70
Lawrence Neal <nealla@lwneal.com>
2019-02-06 23:09:35 -0800

    Increment version
    TODO: automate version number tagging in the top navigation bar

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import subprocess
 
 
 setup(name='gnomehat',
-    version='0.7.4',
+    version='0.7.5',
     description='GnomeHat: easy experiment control',
     author='Larry Neal',
     author_email='nealla@lwneal.com',
