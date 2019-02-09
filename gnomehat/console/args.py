d0375a84e3b80a1a831f3f5f24a3119d08341085
Lawrence Neal <nealla@lwneal.com>
2019-02-08 17:20:26 -0800

    Move arg parsing to console/ package

---
+++ gnomehat/console/args.py
@@ -0,0 +1,57 @@
+import os
+import stat
+import sys
+import uuid
+import os
+import subprocess
+import shutil
+import requests
+import argparse
+import json
+
+COMMANDS = [
+    'start',
+    'stop',
+    'restart',
+    'status',
+    'demo',
+    'logs',
+    'run',
+    'python',
+]
+
+
+def print_usage():
+    print('\n\t\tGnomehat: the simple experiment tracker')
+    print('Command-line interface')
+    print('\nUsage:')
+    print('\tgnomehat start [experiments_dir]')
+    print('\tgnomehat stop')
+    print('\tgnomehat restart')
+    print('\tgnomehat status')
+    print('\tgnomehat logs')
+    print('\tgnomehat doctor')
+    print('\tgnomehat run [-n namespace] [-m message] <command...>')
+    print('')
+    print('start: Starts a gnomehat_server daemon and one gnomehat_worker per GPU')
+    print('stop: Kills all gnomehat daemons on this machine')
+    print('restart: Alias for gnomehat stop && gnomehat start')
+    print('status: Prints all running gnomehat processes')
+    print('logs: Tails log files for all gnomehat daemons')
+    print('doctor: Checks for common configuration problems')
+    print('run: Executes a shell command as a Gnomehat experiment. Requires an active server.')
+    print('\t-n namespace: Optional namespace to organize experiments. Defaults to "default"')
+    print('\t-m message: Optional comment string to remember why you ran this experiment')
+    print('Note: "gnomehat python" is an alias to "gnomehat run python"')
+    print('')
+
+
+def parse_gnomehat_args(argv):
+    for i, arg in enumerate(argv):
+        if arg in COMMANDS:
+            command = argv[i]
+            remaining_args = argv[1:i] + argv[i+1:]
+            break
+    else:
+        raise ValueError('Invalid command')
+    return command, remaining_args
