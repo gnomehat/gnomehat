02efef610a232b13e700dbb24bd469bd50c439af
Lawrence Neal <nealla@lwneal.com>
2019-09-13 11:12:34 -0700

    Increment version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import subprocess
 
 
 setup(name='gnomehat',
-    version='0.7.14',
+    version='0.7.15',
     description='GnomeHat: easy experiment control',
     author='Larry Neal',
     author_email='nealla@lwneal.com',
